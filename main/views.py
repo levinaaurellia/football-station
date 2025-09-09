d7850d98ca778a19faca7ed0f2a0afcfb61e0545
levinaaurellia <emailkamu@example.com>
2025-09-09 20:09:14 +0700

    minor update

--- main/views.py
+++ main/views.py
@@ -4,7 +4,8 @@ def show_main(request):
     context = {
         'app_name' : 'Football Station',
         'name': 'Levina Aurellia',
-        'class': 'PBP D'
+        'class': 'PBP D',
+        'npm': '2406356776'
     }
 
     return render(request, "main.html", context)
